c983e51406eab4d92f2592c2c0a871d83c11d2d0
Türkan Rişvan <turkanrisvan887@gmail.com>
2023-09-05 22:39:07 +0300

    C# Yazılım Kampı

---
+++ YAZILIM KAMPI/YAZILIM KAMPI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YAZILIM_KAMPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
